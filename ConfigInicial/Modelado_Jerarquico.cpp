b7a7110450b81df8b6e95e4f260b4fd459f74296
Berenice Jacinto <berenice.jacinto.robledo@gmail.com>
2026-03-01 16:32:45 -0600

    Previo 5 - Entrega final

--- ConfigInicial/Modelado_Jerarquico.cpp
+++ ConfigInicial/Modelado_Jerarquico.cpp
@@ -1,6 +1,6 @@
 /* Jacinto Robledo Valeria Berenice
 * No. de Cuenta: 32005797-3
-* Fecha: 27/02/2025
+* Fecha: 01/03/2025
 * Practica 5: Modelado Jerarquico
 /*/
 
@@ -26,9 +26,15 @@ movY = 0.0f,
 movZ = -5.0f,
 rot = 0.0f;
 
-//For model
+//For model para mover las articulaciones
 float	hombro = 0.0f, //nos va a permitir mover el brazo
-codo = 0.0f; //nos va a permitir mover el antebrazo
+codo = 0.0f, //nos va a permitir mover el antebrazo
+muneca = 0.0f, //nos va a permitir mover la palma
+dedo1 = 0.0f,
+dedo2 = 0.0f,
+dedo3 = 0.0f,
+dedo4 =0.0f;
+
 
 
 int main() {
@@ -226,7 +232,57 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 36);//B
 
 
+		//model palma
+		model = glm::translate(modelTemp, glm::vec3(1.0f, 0.0f, 0.0f)); //movemos la palma a la posición del antebrazo
+		model = glm::rotate(model, glm::radians(muneca), glm::vec3(1.0f, 0.0f, 0.0f));
+		modelTemp2 = modelTemp = model = glm::translate(model, glm::vec3(0.25f, 0.0f, 0.0f)); //guardamos la información de la palma y lo movemos a la posición de la palma
+		model = glm::scale(model, glm::vec3(0.5f, 1.0f, 1.0f));
+		color = glm::vec3(1.0f, 1.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//C
 
+		
+		//model dedo 1 A
+		model = glm::translate(modelTemp, glm::vec3(0.25f, 0.35f, 0.375f)); //movemos el dedo a la posición de la palma
+		model = glm::rotate(model, glm::radians(dedo1), glm::vec3(0.0f, 0.0f, 1.0f));
+		modelTemp = model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.25f));
+		color = glm::vec3(0.0f, 1.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//D
+
+		
+		//model dedo 1 B
+		model = glm::translate(modelTemp, glm::vec3(0.5f, 0.0f, 0.0f)); //movemos el dedo a la posición de la palma
+		model = glm::rotate(model, glm::radians(dedo2), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f)); 
+		model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.25f));
+		color = glm::vec3(1.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//E
+
+		//model dedo 2 A
+		model = glm::translate(modelTemp2, glm::vec3(0.25f, 0.35f, -0.375f)); //movemos el dedo a la posición de la palma
+		model = glm::rotate(model, glm::radians(dedo3), glm::vec3(0.0f, 0.0f, 1.0f));
+		modelTemp = model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.25f));
+		color = glm::vec3(0.0f, 1.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//F
+
+		//model dedo 2 B
+		model = glm::translate(modelTemp, glm::vec3(0.5f, 0.0f, 0.0f)); //movemos el dedo a la posición de la palma
+		model = glm::rotate(model, glm::radians(dedo4), glm::vec3(0.0f, 0.0f, 1.0f));
+		model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 0.3f, 0.25f));
+		color = glm::vec3(1.0f, 0.0f, 1.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glDrawArrays(GL_TRIANGLES, 0, 36);//G
 
 
 		glBindVertexArray(0);
@@ -271,6 +327,26 @@ int main() {
 		 codo += 0.18f;
 	 if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS)
 		 codo -= 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
+		 muneca += 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS)
+		 muneca -= 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+		 dedo1 += 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+		 dedo1 -= 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+		 dedo2 += 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+		 dedo2 -= 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+		 dedo3 += 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+		 dedo3 -= 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+		 dedo4 += 0.18f;
+	 if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS)
+		 dedo4 -= 0.18f;
  }
 
 
